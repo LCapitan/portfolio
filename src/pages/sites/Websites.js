c0980deacbd74a309960aaf02ba3e2c95e79970e
Austin Melendez <AustinJames17@icloud.com>
2021-07-09 08:19:20 -0400

    - fix mobile and tablet styles

--- src/pages/sites/Websites.js
+++ src/pages/sites/Websites.js
@@ -13,56 +13,48 @@ class Work extends Component {
           </div>
 
           <div className="TwoCol">
-            <div>
-              <div className="ProjectDetails">
-                <h2>i2Verify</h2>
-                <p>site redesign using React and Next.js, on Prismic CMS</p>
-                <a href="https://i2verify.com/" className="Button" target="_blank" rel="noopener noreferrer">
-                  view site
-                  <span>
+            <div className="ProjectDetails">
+              <h2>i2Verify</h2>
+              <p>site redesign using React and Next.js, on Prismic CMS</p>
+              <a href="https://i2verify.com/" className="Button" target="_blank" rel="noopener noreferrer">
+                view site
+                <span>
                   <FontAwesomeIcon icon="arrow-right"/>
                 </span>
-                </a>
-              </div>
+              </a>
             </div>
 
-            <div>
-              <div className="ProjectDetails">
-                <h2>Joslin Diabetes Center</h2>
-                <p>site redesign using CSS Grid and Webpack, on Drupal CMS</p>
-                <a href="https://www.kansashealthsystem.com/" className="Button" target="_blank" rel="noopener noreferrer">
-                  view site
-                  <span>
+            <div className="ProjectDetails">
+              <h2>Joslin Diabetes Center</h2>
+              <p>site redesign using CSS Grid and Webpack, on Drupal CMS</p>
+              <a href="https://www.joslin.org/" className="Button" target="_blank" rel="noopener noreferrer">
+                view site
+                <span>
                   <FontAwesomeIcon icon="arrow-right"/>
                 </span>
-                </a>
-              </div>
+              </a>
             </div>
 
-            <div>
-              <div className="ProjectDetails">
-                <h2>TUKHS</h2>
-                <p>site redesign using Bootstrap, on Sitecore CMS</p>
-                <a href="https://www.kansashealthsystem.com/" className="Button" target="_blank" rel="noopener noreferrer">
-                  view site
-                  <span>
+            <div className="ProjectDetails">
+              <h2>TUKHS</h2>
+              <p>site redesign using Bootstrap, on Sitecore CMS</p>
+              <a href="https://www.kansashealthsystem.com/" className="Button" target="_blank" rel="noopener noreferrer">
+                view site
+                <span>
                   <FontAwesomeIcon icon="arrow-right"/>
                 </span>
-                </a>
-              </div>
+              </a>
             </div>
 
-            <div>
-              <div className="ProjectDetails">
-                <h2>Curry College</h2>
-                <p>site redesign using Bootstrap, built on Ingeniux CMS</p>
-                <a href="https://www.curry.edu/" className="Button" target="_blank" rel="noopener noreferrer">
-                  view site
-                  <span>
+            <div className="ProjectDetails">
+              <h2>Curry College</h2>
+              <p>site redesign using Bootstrap, built on Ingeniux CMS</p>
+              <a href="https://www.curry.edu/" className="Button" target="_blank" rel="noopener noreferrer">
+                view site
+                <span>
                   <FontAwesomeIcon icon="arrow-right"/>
                 </span>
-                </a>
-              </div>
+              </a>
             </div>
           </div>
         </div>
